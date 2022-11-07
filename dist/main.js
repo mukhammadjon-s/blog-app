d414c752f56870cb14df88613a128bd65249680e
Malika Bakhtiyorova <malikabakhtiyorova0@gmail.com>
2022-11-07 14:48:00 +0500

    init server

--- dist/main.js
+++ dist/main.js
@@ -3,14 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+require("reflect-metadata");
 dotenv_1.default.config();
-const app = (0, express_1.default)();
-const port = process.env.PORT;
-app.get('/', (req, res) => {
+var app = express_1.default();
+var port = process.env.PORT;
+app.get('/', function (req, res) {
     res.send('bismillah');
 });
-app.listen(port, () => {
-    return `⚡️[server]: Server is running at http://localhost:${port}`;
+app.listen(port, function () {
+    console.log("[server]: Server is running at http://localhost:" + port);
 });
--- main.ts
+++ main.ts
@@ -11,5 +11,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  return `[server]: Server is running at http://localhost:${port}`;
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
