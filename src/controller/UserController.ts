7f808bf36ec5914b6427a358f5626d3f8eccac83
Malika Bakhtiyorova <malikabakhtiyorova0@gmail.com>
2022-11-08 21:14:59 +0500

    config db, test

---
+++ src/controller/UserController.ts
@@ -0,0 +1,31 @@
+import {getRepository} from "typeorm";
+import {NextFunction, Request, Response} from "express";
+import {User} from "../entity/User";
+
+export class UserController {
+    
+    private userRepository = getRepository(User);
+    
+    async all(request: Request, response: Response, next: NextFunction) {
+        return this.userRepository.find();
+    }
+    
+    async one(request: Request, response: Response, next: NextFunction) {
+        return this.userRepository.findOne(request.params.id);
+    }
+    
+    async save(request: Request, response: Response, next: NextFunction) {
+        return this.userRepository.save({
+        firstName: "Phantom",
+        pass: 'password',
+        lastName: "Assassin",
+        age: 24
+    });
+    }
+    
+    async remove(request: Request, response: Response, next: NextFunction) {
+        const userToRemove = await this.userRepository.findOne(request.params.id);
+        await this.userRepository.remove(userToRemove);
+    }
+    
+}
